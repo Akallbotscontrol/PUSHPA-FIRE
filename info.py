9795fbe9660591dd366af88e1ba08a7b429ad0f1
Ak all bots Control <akallbotscontrol@gmail.com>
2024-12-17 13:38:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://iili.io/2WTDOGa.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/8uq.jpg https://envs.sh/8uw.jpg https://envs.sh/8uP.jpg https://envs.sh/8ub.jpg')).split()
 
 
 # Admins & Users
